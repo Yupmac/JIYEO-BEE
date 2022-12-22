f81cbba6bbdae4cbc95fd0d526bb1cb47fea2e66
Yupmac <hohobanjang@gmail.com>
2022-12-22 23:08:45 +0900

    feat: 컴포넌트 추가 및 폴더 정리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import TodoList from "./pages/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/todo" element={<TodoList />} />
+    </Routes>
+  );
 }
 
 export default App;
